9eb50908fa6fc46577c1bdf057f9f160bb86f266
Depkstha <dharmarajshrestha9870@gmail.com>
2023-08-21 14:55:11 +0545

    first commit

---
+++ resources/views/components/frontend-layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    {{-- Navbar Section --}}
+    <x-navbar />
+
+    <div class="main-content">
+        {{$slot}}
+    </div>
+
+    {{-- Footer Section --}}
+    <x-footer />
+
+</body>
+
+</html>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-frontend-layout>
+
+    {{-- Carousel Section --}}
+    <x-carousel />
+
+    {{-- Event section --}}
+    <x-eventlist />
+
+    {{-- Event section --}}
+    <x-movielist />
+
+</x-frontend-layout>
